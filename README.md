59375d9577bdac1ecfc9f40c40936edb641e0283
ms10 <michaelsongxx@gmail.com>
2023-11-23 13:27:44 -0500

    billing API

--- README.md
+++ README.md
@@ -7,3 +7,24 @@ Using official mongodb docker image
 
 ## Application Container
 Python + Flask
+
+## Usage
+Do `docker-compose up --build` to launch
+
+## Test APIs
+I choose to use Postman for API testing
+This is how ChatGPT instruct me to use Postman for API testing:
+
+1. **Install Postman**: If you don't already have Postman, you can download and install it from [the Postman website](https://www.postman.com/downloads/).
+2. **Create a New Request**:
+    - Open Postman.
+    - Click on the "New" button and select "Request".
+    - Give your request a name and optionally create a collection to organize your requests.
+3. **Set Up and Send Requests**:
+    - Enter your API URL in the request URL field. For example, `http://localhost:5000/api/billing/transaction/` for fetching all transactions.
+    - Select the appropriate HTTP method (GET, POST, PUT, DELETE) based on the API endpoint you are testing.
+    - For POST and PUT requests, you may need to add request body data. Go to the "Body" tab, select "raw", and choose "JSON" from the dropdown. Enter your JSON payload here.
+    - Click "Send" to make the request and view the response in the lower section of the Postman window.
+4. **Analyze the Response**:
+    - After sending the request, Postman will display the server's response, including status code, headers, and body.
+    - Review the response to ensure that it matches your expectations.
--- app/app.py
+++ app/app.py
@@ -1,16 +1,83 @@
-from flask import Flask, jsonify
+import datetime
+from flask import Flask, jsonify, request, Response
 from pymongo import MongoClient
+from bson import ObjectId
+
+def serialize_doc(doc):
+    if "_id" in doc:
+        doc["_id"] = str(doc["_id"])
+    return doc
 
 app = Flask(__name__)
-
 client = MongoClient("mongodb://root:6156_project@mongodb:27017/")
-db = client.testdb
+db = client.rentaldb
+billing_collection = db.billing # store billing info
+transaction_collection = db.transactions # store transaction info by apt_id
 
-@app.route('/')
-def hello_world():
-    db.testcollection.insert_one({"message": "Hello, World!"})
-    message = db.testcollection.find_one({"message": "Hello, World!"})
-    return jsonify(message)
+@app.route('/api/billing/transaction/', methods=['GET', 'POST'])
+def transactions():
+    pass
+
+@app.route('/api/billing/transaction/<string:transaction_id>', methods=['GET', 'PUT', 'DELETE'])
+def transaction(transaction_id):
+    pass
+
+@app.route('/api/billing/transaction/apt/<string:apt_num>', methods=['GET', 'DELETE', 'POST'])
+def insert_billing(apt_num):
+    if request.method == 'POST':
+        """Insert billing info into MongoDB(apt_id, rental price, rental start date, lease period), return the id of the inserted document"""
+        try:
+            # Parse the JSON data from request
+            data = request.json
+            rental_price = data['rental_price']
+            rental_start_time = data.get('rental_start_time', datetime.datetime.now())
+            lease_period = data.get('lease_period', 12)
+
+            # Insert into MongoDB
+            billing_info = {
+                "apartment_id": apt_num,
+                "rental_price": rental_price,
+                "rental_start_time": rental_start_time,
+                "lease_period": lease_period # in months
+            }
+            result = billing_collection.insert_one(billing_info)
+
+            return jsonify({"message": "Billing info inserted successfully", "id": str(result.inserted_id)}), 201
+
+        except Exception as e:
+            # Handle errors
+            return jsonify({"error": str(e)}), 500
+    
+    elif request.method == 'GET':
+        """Get billing info from MongoDB by apartment id"""
+        try:
+            # Get billing info from MongoDB
+            billing_info = billing_collection.find_one({"apartment_id": apt_num})
+            if billing_info:
+                billing_info = serialize_doc(billing_info)
+                return jsonify(billing_info), 200
+            else:
+                return jsonify({"error": "Billing info not found"}), 404
+
+        except Exception as e:
+            # Handle errors
+            return jsonify({"error": str(e)}), 500
+    
+    elif request.method == 'DELETE':
+        """Delete billing info from MongoDB by apartment id"""
+        try:
+            # Delete billing info from MongoDB
+            billing_info = billing_collection.find_one({"apartment_id": apt_num})
+            if billing_info:
+                billing_collection.delete_one({"apartment_id": apt_num})
+                return jsonify({"message": "Billing info deleted successfully"}), 200
+            else:
+                return jsonify({"error": "Billing info not found"}), 404
+
+        except Exception as e:
+            # Handle errors
+            return jsonify({"error": str(e)}), 500
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
